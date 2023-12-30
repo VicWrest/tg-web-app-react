94c83ab9cf1a67955a0cba33b2eea9bb66e3eb8d
VicWrest <cyganov0206@gmail.com>
2023-12-30 21:25:59 +1000

    with validation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import {useEffect} from 'react'
 import { useTelegram } from './hooks/useTelegram';
 import Header from './components/Header/Header';
+import {Route, Routes} from 'react-router-dom';
+import ProductList from './components/ProductList/productList';
+import Form from './components/Form/form';
 
 function App() {
   const {onToggleButton, tg} = useTelegram();
@@ -12,10 +15,17 @@ function App() {
   
   return (
     <div className="App">
-      <Header/>
-      <button onClick={onToggleButton}>Toggle</button>
+    <Header/>
+      <Routes>
+        <Route index element={<ProductList/>}/>
+        <Route path={'form'} element={<Form/>}/>
+      </Routes>
+      
     </div>
   );
+  /*<Route index element={<ProductList/>}/> - по корневому пути запускается productList
+  <Route path = {'form'} element={<Form/>}/> - по url=form - запускается компонент Form
+  */
 }
 
 export default App;
---
+++ src/components/ProductList/productList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from '../Button/Button'
+import { useTelegram } from '../../hooks/useTelegram';
+import './productList.css';
+
+const ProductList = () => {
+  return (
+    <div>
+        productList
+    </div>
+  );
+}
+
+export default ProductList;
